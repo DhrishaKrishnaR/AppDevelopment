eba95e08b157d333e2b9d7d23d23e77a698ddbb5
DhrishaKrishnaR <110531584+DhrishaKrishnaR@users.noreply.github.com>
2023-09-27 13:29:35 +0530

    Add files via upload

---
+++ Day10/job_searching_portal/src/main/java/com/example/demo/entity/Login.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Login {
+	private Long userId;
+	private String userName;
+	private String userpassword;
+	
+
+}
